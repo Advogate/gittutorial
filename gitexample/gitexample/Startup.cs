9825fc39ceae9128ada8aee955caa34d8158b0fa
templarcodedev <gbolaabidoye@gmail.com>
2018-12-29 12:39:00 +0100

    added an empty asp.net project

---
+++ gitexample/gitexample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitexample.Startup))]
+namespace gitexample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
